2c6439574f457d04d635f0dcc5201d4d175f4980
BurshteynAnton <burshteynmail@gmail.com>
2024-09-17 09:50:28 +0300

    still have troubles

--- src/main/java/telran/java53/ForumServiceApplication.java
+++ src/main/java/telran/java53/ForumServiceApplication.java
@@ -2,8 +2,13 @@ package telran.java53;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"telran.java53"})
+@EnableJpaRepositories(basePackages = {"telran.java53.post.dao", "telran.java53.registration.token", "telran.java53.accounting.dao"})
+@EntityScan(basePackages = {"telran.java53.post.model", "telran.java53.registration.token", "telran.java53.accounting.model"})
+
 public class ForumServiceApplication {
 
 	public static void main(String[] args) {
